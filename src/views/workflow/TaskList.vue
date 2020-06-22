e6cf4e68028d956db51cf5348749826625a94244
zouqing1 <17612169310@163.com>
2020-06-22 16:02:24 +0800

    scroll bar

--- src/views/workflow/TaskList.vue
+++ src/views/workflow/TaskList.vue
@@ -1,14 +1,9 @@
 <template>
-  <div :class="theme=='Light'?'no-search-content-box advice-apply adaL-light flex-height-table':'no-search-content-box advice-apply flex-height-table'">
+  <div
+    :class="theme=='Light'?'no-search-content-box advice-apply adaL-light flex-height-table':'no-search-content-box advice-apply flex-height-table'"
+  >
     <div class="table-bg-box">
-      <el-table
-        ref="table"
-        :data="taskToView"
-        border
-        stripe
-        style="width: 100%"
-        height="100%"
-      >
+      <el-table ref="table" :data="taskToView" border stripe style="width: 100%" height="100%">
         <el-table-column type="selection" width="45" />
         <el-table-column :label="$t('comm.operation')" width="80" align="center">
           <template slot-scope="scope">
@@ -32,7 +27,10 @@
           align="center"
         >
           <template slot-scope="scope">
-            <a class="link" @click.prevent="handleProcessInstClick(scope.row.processInstId)">{{ scope.row.processInstId }}</a>
+            <a
+              class="link"
+              @click.prevent="handleProcessInstClick(scope.row.processInstId)"
+            >{{ scope.row.processInstId }}</a>
           </template>
         </el-table-column>
         <el-table-column
@@ -64,34 +62,40 @@
         />
       </el-table>
     </div>
-    <el-dialog id="processDialog" :title="$t('workflow.detailedProcess') + processInstId" :visible.sync="dialogTableVisible" custom-class="dialog-drag" class="content-dialog-box">
+    <el-dialog
+      id="processDialog"
+      :title="$t('workflow.detailedProcess') + processInstId"
+      :visible.sync="dialogTableVisible"
+      custom-class="dialog-drag"
+      class="content-dialog-box"
+    >
       <process-detail :key="processInstId" :process-inst-id="processInstId" />
     </el-dialog>
   </div>
 </template>
 <script>
-
-import { getTasks, agreeTasks } from '@/api/base'
-import ProcessDetail from '@/components/ProcessDetail'
-import { mapGetters } from 'vuex'
-import $ from 'jquery'
-import 'jquery.nicescroll'
+import { getTasks, agreeTasks } from "@/api/base";
+import ProcessDetail from "@/components/ProcessDetail";
+import { mapGetters } from "vuex";
+import $ from "jquery";
+import "jquery.nicescroll";
 export default {
-  name: 'TaskList',
+  name: "TaskList",
   components: {
     ProcessDetail
   },
   data() {
     return {
-      theme: localStorage.getItem('theme') !== 'Dark' ? 'Light' : '',
+      theme: localStorage.getItem("theme") !== "Dark" ? "Light" : "",
       dialogVisible: false,
-      name: '',
-      scrollColr: localStorage.getItem('theme') !== 'Dark' ? '#D8E0E8' : '#5A5E63',
+      name: "",
+      scrollColr:
+        localStorage.getItem("theme") !== "Dark" ? "#D8E0E8" : "#5A5E63",
       isLoading: true,
-      activeNames: ['1'],
+      activeNames: ["1"],
       taskList: [],
       taskToView: [],
-      processInstId: '',
+      processInstId: "",
       dialogTableVisible: false,
       multipleSelection: [],
       searchParam: {
@@ -100,22 +104,30 @@ export default {
         pageSize: 10, // 每页条数
         totalRecord: null, // 总条数
         params: {
-          procInstId: '',
-          procDefName: '',
-          procInstName: ''
+          procInstId: "",
+          procDefName: "",
+          procInstName: ""
         }
       }
-    }
+    };
   },
   created() {
-    this.fetchData()
+    this.fetchData();
+  },
+  watch: {
+    dialogTableVisible: function() {
+      this.$nextTick(function() {
+        this.getScrollBar();
+      });
+    }
   },
   mounted() {
     // this.getAdrApproveList()
     // 拖拽
-    this.draggable()
+    this.draggable();
     // 缩放
-    this.resizable()
+    this.resizable();
+    this.getScrollBar();
     // 拖拽
     // $('.dialog-drag').draggable({
     //   cursor: 'move',
@@ -127,58 +139,74 @@ export default {
     // })
   },
   Computed: {
-    ...mapGetters([
-      'searchParams'
-    ])
+    ...mapGetters(["searchParams"])
   },
   methods: {
     draggable() {
-      $('.dialog-drag').draggable({
-        cursor: 'move',
-        handle: '.el-dialog__header',
+      $(".dialog-drag").draggable({
+        cursor: "move",
+        handle: ".el-dialog__header",
         refreshPositions: true,
-        containment: 'parent',
+        containment: "parent",
         stop() {
-          $('.el-dialog__body').getNiceScroll().resize()
+          $(".el-dialog__body")
+            .getNiceScroll()
+            .resize();
         }
-      })
+      });
     },
     resizable() {
-      $('.dialog-drag').resizable({
+      $(".dialog-drag").resizable({
         aspectRatio: false,
         minHeight: 150,
-        containment: 'parent',
-        stop: function (event, ui) {
-          $('.footer-box').addClass('drag-table-height')
-          $('.el-table__body-wrapper').getNiceScroll().resize()
-          $('.el-dialog__body').getNiceScroll().resize()
+        containment: "parent",
+        stop: function(event, ui) {
+          $(".footer-box").addClass("drag-table-height");
+          $(".el-table__body-wrapper")
+            .getNiceScroll()
+            .resize();
+          $(".el-dialog__body")
+            .getNiceScroll()
+            .resize();
         }
-      })
+      });
     },
-    fetchData: async function () {
-      const res = await getTasks(this.searchParam.params)
+    fetchData: async function() {
+      const res = await getTasks(this.searchParam.params);
       if (res && res.success) {
         if (res.datas && res.datas.taskList) {
-          const _this = this
-          let ret = res.datas.taskList
+          const _this = this;
+          let ret = res.datas.taskList;
           if (_this.searchParam.params.procInstId) {
             ret = ret.filter(item => {
-              return item.processInstId.indexOf(_this.searchParam.params.procInstId) > -1
-            })
+              return (
+                item.processInstId.indexOf(
+                  _this.searchParam.params.procInstId
+                ) > -1
+              );
+            });
           }
           if (_this.searchParam.params.procInstName) {
             ret = ret.filter(item => {
-              return item.processName.indexOf(_this.searchParam.params.procInstName) > -1
-            })
+              return (
+                item.processName.indexOf(
+                  _this.searchParam.params.procInstName
+                ) > -1
+              );
+            });
           }
-          this.taskList = ret
-          this.searchParam.totalRecord = ret.length
-          const start = (this.searchParam.pageNo - 1) * this.searchParam.pageSize
-          this.taskToView = this.taskList.slice(start, start + this.searchParam.pageSize)
+          this.taskList = ret;
+          this.searchParam.totalRecord = ret.length;
+          const start =
+            (this.searchParam.pageNo - 1) * this.searchParam.pageSize;
+          this.taskToView = this.taskList.slice(
+            start,
+            start + this.searchParam.pageSize
+          );
         }
       }
-      this.tableDolayout(this.$refs['table'])
-      this.isLoading = false
+      this.tableDolayout(this.$refs["table"]);
+      this.isLoading = false;
     },
     handleBtnExec(row) {
       this.$router.push({
@@ -188,46 +216,55 @@ export default {
           processInstId: row.processInstId,
           id: row.businessKey
         }
-      })
+      });
     },
-    handleProcessInstClick: function (processInstId) {
-      this.processInstId = processInstId
-      this.dialogTableVisible = true
+    handleProcessInstClick: function(processInstId) {
+      this.processInstId = processInstId;
+      this.dialogTableVisible = true;
+      this.getScrollBar();
     },
     // 申请
     onApply(row) {
-      console.log('row', row)
-      this.name = row.itemName
-      this.dialogVisible = true
+      console.log("row", row);
+      this.name = row.itemName;
+      this.dialogVisible = true;
     },
     // 确认
     onConfirm() {
-      this.dialogVisible = false
+      this.dialogVisible = false;
       this.$router.push({
-        path: '/ada/edit',
+        path: "/ada/edit",
         query: {
           name: this.form.template
         }
-      })
+      });
     },
     // 查看
-    onLook(row) {
-
-    },
+    onLook(row) {},
     // 编辑
-    onEdit(row) {
-
-    },
+    onEdit(row) {},
     // 撤销
-    onRevoke(row) {
-
-    },
+    onRevoke(row) {},
     // 删除
     onDeleted(index) {
-      console.log('row', index)
+      console.log("row", index);
+    },
+    getScrollBar() {
+      $(".el-dialog__body").niceScroll({
+        cursorcolor: this.scrollColr,
+        cursoropacitymin: 0, // 当滚动条是隐藏状态时改变透明度, 值范围 1 到 0
+        cursoropacitymax: 1, // 当滚动条是显示状态时改变透明度, 值范围 1 到 0
+        cursorwidth: "8px", // 滚动条的宽度，单位：便素
+        cursorborder: `1px solid ${this.scrollColr}`, // CSS方式定义滚动条边框
+        autohidemode: true, // 隐藏滚动条的方式, 可用的值:
+        zindex: 0,
+        railpadding: { top: 0, right: 0, left: 0, bottom: 0 },
+        boxzoom: false,
+        iframeautoresize: false // 在加载事件时自动重置iframe大小
+      });
     }
   }
-}
+};
 </script>
 <style lang="scss">
 #processDialog .el-dialog {
